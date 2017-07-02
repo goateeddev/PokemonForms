00e4b7f778ac0d406f74ea68fb799b869fd2e11c
goateeddev <goateeddev@gmail.com>
2017-07-02 18:25:16 +0100

    Added Pokemon Forms Application to repo

---
+++ AppSettings/AppSettings.cs
@@ -0,0 +1,38 @@
+﻿namespace AppSettings
+{
+    public static class DefaultFilePaths
+    {
+        public static string pokedex_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\Pokedex.csv";
+        public static string pokedex_empty_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\Pokedex_empty.csv";
+
+        public static string pokemon_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\pokemon.csv";
+        public static string types_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\types.csv";
+        public static string moves_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\moves.csv";
+        public static string balls_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\balls.csv";
+
+        public static string pokemon_types_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\pokemon_types.csv";
+        public static string pokemon_moves_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\DataLayer\csv\pokemon_moves.csv";
+
+        public static string pokemon_image_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\PokemonFormsApplication\images\pokemon\";
+        public static string image_path = @"C:\Users\Damaani\Documents\GitHub\VisualStudio\Csharp\PokemonFormsApplication\PokemonFormsApplication\images\";
+    }
+
+    public static class DefaultsStrings
+    {
+        public static string msg_welcome = "Welcome to Pokemon Forms Application!";
+        public static string msg_starter = "You have a choice of 3 starter pokemon!";
+        public static string msg_begin = "Your adventure is about to begin...";
+        public static string msg_poff = "Piss off then.";
+        
+        public static string get_gender = "Are you a boy or are you a girl?";
+        public static string get_name = "And what is your name?";
+        public static string get_embark = "Are you ready to embark on a pokemon adventure?!";
+
+        public static string show_starter_choices = "1 = Bulbasaur" + "\n" + "2 = Charmander" + "\n" + "3 = Squirtle";
+
+        public static string error_yes_no = "Huh? It's a yes or no question bro";
+        public static string error_not_option = "Thats not one of the options I gave you...";
+        public static string error_username = "I don't like that username. Choose another one.";
+        public static string error_no_balls = "You have none left. Choose a different one.";
+    }
+}
