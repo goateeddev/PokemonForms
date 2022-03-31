b1c9c277fa569f5372ef8f23dded547d83a5b75c
Goateed Dev <goateeddev@gmail.com>
2022-03-31 20:24:12 +0100

    Update player move and max boundary logic

--- src/Presentation.WPF/MainWindow.xaml.cs
+++ src/Presentation.WPF/MainWindow.xaml.cs
@@ -23,19 +23,22 @@ namespace Presentation.WPF
     public partial class MainWindow : Window
     {
         private readonly DispatcherTimer _dispatcherTimer = new DispatcherTimer();
+        private int footsteps;
+
         public MainWindow()
         {
             InitializeComponent();
+            canvas.Focus();
+            canvas.KeyDown += MovePlayer;
 
-            _dispatcherTimer.Tick += InGameListener;
+            _dispatcherTimer.Tick += GameLifetime;
             _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(20);
             _dispatcherTimer.Start();
-            player.KeyDown += MovePlayer;
         }
 
-        private void InGameListener(object sender, EventArgs e)
+        private void GameLifetime(object sender, EventArgs e)
         {
-
+            
         }
 
         public void MovePlayer(object sender, KeyEventArgs e)
@@ -44,34 +47,38 @@ namespace Presentation.WPF
             {
                 case Key.Left:
                     player.Source = new BitmapImage(new Uri(DefaultFilePaths.image_path + "characters\\player_left.png", UriKind.Relative));
-                    //if (!bLeft) player.Left -= mHorizontal;
-                    //if (IntersectionDetected(CurrentPanelPictureBoxes)) player.Left += mHorizontal;
-                    //bRight = false;
-                    //footsteps++;
+                    if (Canvas.GetLeft(player) > Canvas.GetLeft(land))
+                    {
+                        Canvas.SetLeft(player, Canvas.GetLeft(player) - player.Width);
+                    }
+                    footsteps++;
                     break;
 
                 case Key.Right:
                     player.Source = new BitmapImage(new Uri(DefaultFilePaths.image_path + "characters\\player_right.png", UriKind.Relative));
-                    //if (!bRight) player.Left += mHorizontal;
-                    //if (IntersectionDetected(CurrentPanelPictureBoxes)) player.Left -= mHorizontal;
-                    //bLeft = false;
-                    //footsteps++;
+                    if (Canvas.GetLeft(player) + player.Width < Canvas.GetLeft(land) + land.Width)
+                    {
+                        Canvas.SetLeft(player, Canvas.GetLeft(player) + player.Width);
+                    }
+                    footsteps++;
                     break;
 
                 case Key.Up:
                     player.Source = new BitmapImage(new Uri(DefaultFilePaths.image_path + "characters\\player_back.png", UriKind.Relative));
-                    //if (!bTop) player.Top -= mVertical;
-                    //if (IntersectionDetected(CurrentPanelPictureBoxes)) player.Top += mVertical;
-                    //bBottom = false;
-                    //footsteps++;
+                    if (Canvas.GetTop(player) > Canvas.GetTop(land))
+                    {
+                        Canvas.SetTop(player, Canvas.GetTop(player) - player.Height);
+                    }
+                    footsteps++;
                     break;
 
                 case Key.Down:
                     player.Source = new BitmapImage(new Uri(DefaultFilePaths.image_path + "characters\\player_front.png", UriKind.Relative));
-                    //if (!bBottom) player.Top += mVertical;
-                    //if (IntersectionDetected(CurrentPanelPictureBoxes)) player.Top -= mVertical;
-                    //bTop = false;
-                    //footsteps++;
+                    if (Canvas.GetTop(player) + player.Height < Canvas.GetTop(land) + land.Height)
+                    {
+                        Canvas.SetTop(player, Canvas.GetTop(player) + player.Height);
+                    }
+                    footsteps++;
                     break;
 
                 default:
